7992031c65fd4a8459598194f84b4d05de9c3d1b
contilorenzo <lorenzo.conti.bg@gmail.com>
2020-09-09 10:24:33 +0200

    Creato modulo Core

---
+++ EsameAngular/src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+  
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpCommunicationsService } from './http-communications/http-communications.service';
+import { HttpClientModule } from '@angular/common/http';
+import {RegistrazioneServerService} from './services/registrazione-server.service';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ],
+  providers: [HttpCommunicationsService,RegistrazioneServerService],
+})
+export class CoreModule { }
